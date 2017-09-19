5866514122998088d3f6f37ef5821e3e1a0b15d3
rajaraman <g.rajaraman0391@gmail.com>
2017-09-19 02:45:20 -0700

    first commit

--- app.js
+++ app.js
@@ -16,13 +16,14 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(lessMiddleware(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'private')));
 
 app.use('/', index);
 //app.use('/users', users);
---
+++ public/javascripts/app.js
@@ -0,0 +1,55 @@
+function myFunction() {
+    var x= document.getElementById('myDIV');
+    var y= document.getElementById('drop');
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+        y.style.display = 'none';
+    } else {
+        x.style.display = 'none';
+        y.style.display = 'block';
+    }
+}
+function showMast() {
+  // var z = document.getElementById('proj3');
+  // z.style.display ='block';
+  // document.getElementById('univ').style.display = 'none';
+  var x= document.getElementById('proj3');
+  var y= document.getElementById('univ');
+  if (x.style.display === 'none') {
+      x.style.display = 'block';
+      y.style.display = 'none';
+  } else {
+      x.style.display = 'none';
+      y.style.display = 'block';
+  }
+}
+function showWork() {
+  // var z = document.getElementById('proj1');
+  // z.style.display ='block';
+  // document.getElementById('work').style.display = 'none';
+
+  var x= document.getElementById('proj1');
+  var y= document.getElementById('work');
+  if (x.style.display === 'none') {
+      x.style.display = 'block';
+      y.style.display = 'none';
+  } else {
+      x.style.display = 'none';
+      y.style.display = 'block';
+  }
+}
+function showBach() {
+  // var z = document.getElementById('proj1');
+  // z.style.display ='block';
+  // document.getElementById('work').style.display = 'none';
+
+  var x= document.getElementById('proj2');
+  var y= document.getElementById('bach');
+  if (x.style.display === 'none') {
+      x.style.display = 'block';
+      y.style.display = 'none';
+  } else {
+      x.style.display = 'none';
+      y.style.display = 'block';
+  }
+}
